118e51541f2539f723600c4a6bf7d08ac5aae13c
Daniel Kleine-Albers <dkleinealbers@nowcast.de>
2015-09-20 12:02:58 +0200

    Use EGit models instead of custom ones. Convert commits.

--- src/main/java/com/dkaedv/glghproxy/controller/ReposController.java
+++ src/main/java/com/dkaedv/glghproxy/controller/ReposController.java
@@ -6,7 +6,13 @@ import java.util.Vector;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.eclipse.egit.github.core.Repository;
+import org.eclipse.egit.github.core.RepositoryBranch;
+import org.eclipse.egit.github.core.RepositoryCommit;
 import org.gitlab.api.GitlabAPI;
+import org.gitlab.api.models.GitlabBranch;
+import org.gitlab.api.models.GitlabCommit;
+import org.gitlab.api.models.GitlabCommitDiff;
 import org.gitlab.api.models.GitlabProject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,8 +22,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.dkaedv.glghproxy.githubentity.Repository;
-import com.dkaedv.glghproxy.githubentity.User;
+import com.dkaedv.glghproxy.converter.GitlabToGithubConverter;
 import com.dkaedv.glghproxy.gitlabclient.GitlabSessionProvider;
 
 @Controller
@@ -53,18 +58,8 @@ public class ReposController {
 
 		GitlabAPI api = gitlab.connect(authorization);
 		List<GitlabProject> projects = api.getAllProjects();
-		List<Repository> repos = new Vector<Repository>();
 		
-		for (GitlabProject p : projects) {
-			Repository repo = new Repository(p.getId(), p.getName(), p.getPathWithNamespace());
-			
-			// JIRA does only display repositories that have an owner with the same name as the name in JIRA
-			// This behaviour seems to be correlating best with the namespace concept in Gitlab
-			repo.setOwner(new User(p.getNamespace().getName()));
-			repos.add(repo);
-		}
-		
-		return repos;
+		return GitlabToGithubConverter.convertRepositories(projects);
 	}
 
 	@RequestMapping("/user/repos")
@@ -77,5 +72,37 @@ public class ReposController {
 		LOG.info("Received request: per_page=" + per_page + ", page=" + page);
 
 		return new Vector<Repository>();
+	}
+	
+	@RequestMapping("/repos/{namespace}/{repo}/branches")
+	@ResponseBody
+	public List<RepositoryBranch> getBranches(
+			@PathVariable String namespace,
+			@PathVariable String repo,
+			@RequestParam String per_page,
+			@RequestParam String page,
+			@RequestHeader("Authorization") String authorization
+			) throws IOException {
+		
+		GitlabAPI api = gitlab.connect(authorization);
+		List<GitlabBranch> glbranches = api.getBranches(namespace + "/" + repo);
+		
+		return GitlabToGithubConverter.convertBranches(glbranches);		
+	}
+	
+	@RequestMapping("/repos/{namespace}/{repo}/commits/{sha}")
+	@ResponseBody
+	public RepositoryCommit getCommit(
+			@PathVariable String namespace,
+			@PathVariable String repo,
+			@PathVariable String sha,
+			@RequestHeader("Authorization") String authorization
+			) throws IOException {
+		
+		GitlabAPI api = gitlab.connect(authorization);
+		GitlabCommit glcommit = api.getCommit(namespace + "/" + repo, sha);
+		List<GitlabCommitDiff> gldiffs = api.getCommitDiffs(namespace + "/" + repo, sha);
+		
+		return GitlabToGithubConverter.convertCommit(glcommit, gldiffs);
 	}
 }
---
+++ src/main/java/com/dkaedv/glghproxy/converter/GitlabToGithubConverter.java
@@ -0,0 +1,115 @@
+package com.dkaedv.glghproxy.converter;
+
+import java.util.List;
+import java.util.Vector;
+
+import org.apache.commons.lang3.StringUtils;
+import org.eclipse.egit.github.core.Commit;
+import org.eclipse.egit.github.core.CommitFile;
+import org.eclipse.egit.github.core.CommitUser;
+import org.eclipse.egit.github.core.Repository;
+import org.eclipse.egit.github.core.RepositoryBranch;
+import org.eclipse.egit.github.core.RepositoryCommit;
+import org.eclipse.egit.github.core.TypedResource;
+import org.eclipse.egit.github.core.User;
+import org.gitlab.api.models.GitlabBranch;
+import org.gitlab.api.models.GitlabCommit;
+import org.gitlab.api.models.GitlabCommitDiff;
+import org.gitlab.api.models.GitlabProject;
+
+public class GitlabToGithubConverter {
+
+	public static RepositoryBranch convertBranch(GitlabBranch glbranch) {
+		RepositoryBranch branch = new RepositoryBranch();
+		branch.setName(glbranch.getName());
+
+		TypedResource commit = new TypedResource();
+		commit.setType(TypedResource.TYPE_COMMIT);
+		commit.setSha(glbranch.getCommit().getId());
+
+		branch.setCommit(commit);
+		return branch;
+	}
+
+	public static List<RepositoryBranch> convertBranches(List<GitlabBranch> glbranches) {
+		List<RepositoryBranch> branches = new Vector<RepositoryBranch>();
+
+		for (GitlabBranch glbranch : glbranches) {
+			RepositoryBranch branch = convertBranch(glbranch);
+			branches.add(branch);
+		}
+		return branches;
+	}
+
+	public static RepositoryCommit convertCommit(GitlabCommit glcommit, List<GitlabCommitDiff> gldiffs) {
+		RepositoryCommit repoCommit = new RepositoryCommit();
+
+		repoCommit.setSha(glcommit.getId());
+
+		Commit commit = new Commit();
+		commit.setMessage(glcommit.getTitle());
+
+		CommitUser commitUser = new CommitUser();
+		commitUser.setName(glcommit.getAuthorName());
+		commitUser.setEmail(glcommit.getAuthorEmail());
+		commitUser.setDate(glcommit.getCreatedAt());
+		commit.setAuthor(commitUser);
+		commit.setCommitter(commitUser);
+
+		repoCommit.setCommit(commit);
+
+		User user = new User();
+		user.setEmail(glcommit.getAuthorEmail());
+		user.setLogin(glcommit.getAuthorName());
+		repoCommit.setAuthor(user);
+		repoCommit.setCommitter(user);
+
+		List<Commit> parents = new Vector<Commit>();
+		for (String parentSha : glcommit.getParentIds()) {
+			Commit parent = new Commit();
+			parent.setSha(parentSha);
+			parents.add(parent);
+		}
+		repoCommit.setParents(parents);
+
+		List<CommitFile> files = new Vector<CommitFile>();
+		for (GitlabCommitDiff diff : gldiffs) {
+			CommitFile file = new CommitFile();
+			file.setFilename(diff.getNewPath());
+
+			int additions = StringUtils.countMatches(diff.getDiff(), "\n+") - StringUtils.countMatches(diff.getDiff(), "\n+++");
+			int deletions = StringUtils.countMatches(diff.getDiff(), "\n-") - StringUtils.countMatches(diff.getDiff(), "\n---");
+			
+			file.setAdditions(additions);
+			file.setDeletions(deletions);
+			
+			files.add(file);
+		}
+		repoCommit.setFiles(files);
+
+		return repoCommit;
+	}
+	
+	public static Repository convertRepository(GitlabProject project) {
+		Repository repo = new Repository();
+		
+		repo.setId(project.getId());
+		repo.setName(project.getName());
+		
+		User user = new User();
+		user.setLogin(project.getNamespace().getName());
+		repo.setOwner(user);
+		
+		return repo;
+	}
+
+	public static List<Repository> convertRepositories(List<GitlabProject> projects) {
+		List<Repository> repos = new Vector<Repository>();
+		
+		for (GitlabProject project : projects) {
+			repos.add(convertRepository(project));
+		}
+		
+		return repos;
+	}
+}
