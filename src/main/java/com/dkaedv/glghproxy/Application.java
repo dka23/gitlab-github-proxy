c3a3b82dc002098871ccb2627b8c9a5de4f76e48
Daniel Kleine-Albers <dkleinealbers@nowcast.de>
2015-09-25 21:59:43 +0200

    Support WAR deployment

--- src/main/java/com/dkaedv/glghproxy/Application.java
+++ src/main/java/com/dkaedv/glghproxy/Application.java
@@ -2,17 +2,24 @@ package com.dkaedv.glghproxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 	
 	@Bean
 	public Jackson2ObjectMapperBuilder jacksonBuilder() {
